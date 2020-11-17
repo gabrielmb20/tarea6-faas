5eb452d38f3217b1d8ab464d1578c1332f517ebd
Gabriel Martinez Barboza <gabriel.mb20@gmail.com>
2020-11-16 19:48:13 -0600

    Tarea 6 FAAS

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/arce/faas
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.13.2
+	go.mongodb.org/mongo-driver v1.4.2
+)
---
+++ www/js/app.js
@@ -0,0 +1,61 @@
+function getAll(entity) {
+	fetch('https://faas-example.netlify.app/api/' + entity)
+	  .then((response) => response.json())
+		.then((data) => {
+			fetch('/template/list/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
+}
+
+function getById(query, entity) {
+	var params = new URLSearchParams(query);
+	fetch('https://faas-example.netlify.app/api/' + entity + '/?id=' + params.get('id'))
+	  .then((response) => response.json())
+		.then((data) => {
+			fetch('/template/detail/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
+}
+
+function home() {
+	fetch('/template/home.html')
+		.then((response) => response.text())
+		.then((template) => {
+			var rendered = Mustache.render(template, {});
+			document.getElementById('content').innerHTML = rendered;
+		});
+}
+
+function init() {
+	router = new Navigo(null, false, '#!');
+	router.on({
+		'/courses': function() {
+			getAll('courses');
+		},
+		'/groups': function() {
+			getAll('groups');
+		},
+		'/professors': function() {
+			getAll('professors');
+		},
+		'/courseById': function(_, query) {
+			getById(query, 'courses');
+		},
+		'/groupById': function(_, query) {
+			getById(query, 'groups');
+		},
+		'/professorById': function(_, query) {
+			getById(query, 'professors');
+		}
+	});
+	router.on(() => home());
+	router.resolve();
+}
