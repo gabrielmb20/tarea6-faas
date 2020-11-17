10fc4640bfe09766ea70a79eb2d9c4e8343bdd6e
Gabriel Martinez Barboza <gabriel.mb20@gmail.com>
2020-11-16 23:35:15 -0600

    Tarea 6 FAAS

--- api/courses/main.go
+++ api/courses/main.go
@@ -22,10 +22,10 @@ type Courses struct {
 	Name      string    `json:"name"`
 	Period string    `json:"period"`
 	Year   int       `json:"year"`
-	Group_Id      ]GroupRef    `json:"group_id"`
-	Code       ]GroupRef `json:"code"`
-	Professor_Id      ]ProfessorRef    `json:"professor_id"`
-	Professor_Name      ]ProfessorRef    `json:"profname"`
+	Group_Id      []GroupRef    `json:"group_id"`
+	Code       []GroupRef `json:"code"`
+	Professor_Id     [] ProfessorRef    `json:"professor_id"`
+	Professor_Name      []ProfessorRef    `json:"profname"`
 }
 
 var items []Courses
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/gabrielmb20/tarea6-faas
-
-go 1.13
-
-require (
-	github.com/aws/aws-lambda-go v1.13.2
-	go.mongodb.org/mongo-driver v1.4.2
-)
